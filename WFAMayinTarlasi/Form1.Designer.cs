d0a9dfb67d3f695a24ccb5886812f3168717d4ed
Emre Sandıkçı <me@emresandikci.com>
2017-06-03 05:03:48 +0300

    init all

---
+++ WFAMayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WFAMayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fltTarla = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblMayinSayisi = new System.Windows.Forms.Label();
+            this.btnYeniOyun = new System.Windows.Forms.Button();
+            this.lblYeniOyun = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fltTarla
+            // 
+            this.fltTarla.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fltTarla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fltTarla.Location = new System.Drawing.Point(12, 56);
+            this.fltTarla.Name = "fltTarla";
+            this.fltTarla.Size = new System.Drawing.Size(261, 210);
+            this.fltTarla.TabIndex = 0;
+            // 
+            // lblMayinSayisi
+            // 
+            this.lblMayinSayisi.AutoSize = true;
+            this.lblMayinSayisi.Location = new System.Drawing.Point(9, 15);
+            this.lblMayinSayisi.Name = "lblMayinSayisi";
+            this.lblMayinSayisi.Size = new System.Drawing.Size(22, 13);
+            this.lblMayinSayisi.TabIndex = 0;
+            this.lblMayinSayisi.Text = "M :";
+            // 
+            // btnYeniOyun
+            // 
+            this.btnYeniOyun.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnYeniOyun.Location = new System.Drawing.Point(115, 6);
+            this.btnYeniOyun.Name = "btnYeniOyun";
+            this.btnYeniOyun.Size = new System.Drawing.Size(36, 31);
+            this.btnYeniOyun.TabIndex = 1;
+            this.btnYeniOyun.Text = " ";
+            this.btnYeniOyun.UseVisualStyleBackColor = true;
+            this.btnYeniOyun.Click += new System.EventHandler(this.btnYeniOyun_Click);
+            this.btnYeniOyun.MouseEnter += new System.EventHandler(this.btnYeniOyun_MouseEnter);
+            this.btnYeniOyun.MouseLeave += new System.EventHandler(this.btnYeniOyun_MouseLeave);
+            // 
+            // lblYeniOyun
+            // 
+            this.lblYeniOyun.AutoSize = true;
+            this.lblYeniOyun.Location = new System.Drawing.Point(157, 15);
+            this.lblYeniOyun.Name = "lblYeniOyun";
+            this.lblYeniOyun.Size = new System.Drawing.Size(53, 13);
+            this.lblYeniOyun.TabIndex = 2;
+            this.lblYeniOyun.Text = "YeniOyun";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(285, 278);
+            this.Controls.Add(this.lblYeniOyun);
+            this.Controls.Add(this.btnYeniOyun);
+            this.Controls.Add(this.lblMayinSayisi);
+            this.Controls.Add(this.fltTarla);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel fltTarla;
+        private System.Windows.Forms.Label lblMayinSayisi;
+        private System.Windows.Forms.Button btnYeniOyun;
+        private System.Windows.Forms.Label lblYeniOyun;
+    }
+}
+
---
+++ WFAMayinTarlasi/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace WFAMayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Global Değişken Tanımlamaları
+        Random Mayinlar = new Random();
+        Button Tiklanan = new Button();
+        SoundPlayer ses = new SoundPlayer();
+        int Bos = 0;
+        int dolu = 0;
+        int Kazanma = 0;
+        #endregion
+       
+        #region Yeni Oyun Metodu
+        /// <summary>
+        /// Yeni oyunu başlatmak için
+        /// </summary>
+        void YeniOyun()
+        {
+            #region Defaul Değerler
+
+            btnYeniOyun.BackgroundImage = Image.FromFile("../../images/gülen.png");
+            btnYeniOyun.BackgroundImageLayout = ImageLayout.Stretch;
+            Bos = 0;
+            dolu = 0;
+            #endregion
+
+            int MayinEkle = Mayinlar.Next(0, 35);
+            int a = 9 * 9;
+            
+
+            fltTarla.Controls.Clear();
+            for (int i = 0; i <= a - 2; i++)
+            {
+                Button btnMayin = new Button();
+                btnMayin.Width = 25;
+                btnMayin.Height = 25;
+                btnMayin.Margin = new System.Windows.Forms.Padding(0);
+                fltTarla.Controls.Add(btnMayin);
+                btnMayin.Click += btnMayin_Click;
+            }
+
+            foreach (Control Oku in fltTarla.Controls)
+            {
+                Button Mekle = Oku as Button;
+                Mekle.Tag = Mayinlar.Next(1, 45);
+            }
+            foreach (Control k in fltTarla.Controls)
+            {
+                Button deger = k as Button;
+                int sayi = int.Parse(deger.Tag.ToString());
+                if (sayi % 2 == 0)
+                {
+                    Bos++;
+                }
+                else
+                {
+                    dolu++;
+                }
+            }
+            lblMayinSayisi.Text = "M:" + " " + dolu + " " + "B:" + " " + Bos;
+        } 
+        #endregion
+
+        #region Form Yüklenmesindeki Olaylar
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblYeniOyun.Visible = false;
+            YeniOyun();
+        } 
+        #endregion
+
+        #region Mayın Var mı yok mu Kontrol 
+       
+        void btnMayin_Click(object sender, EventArgs e)
+        {
+            Button Tiklanan = sender as Button;
+            int GelenDeger = int.Parse(Tiklanan.Tag.ToString());
+
+            #region Kazanma Olayı
+            if (GelenDeger % 2 == 0)
+            {
+                Tiklanan.BackColor = Color.Green;
+                Tiklanan.Enabled = false;
+                Kazanma++;
+                #region Tık Sesi
+                ses.SoundLocation = "../../sesler/tıklayınca.wav";
+                ses.Play();
+               
+                #endregion
+
+                if (Kazanma==Bos)
+                {
+                    #region Kazanma Sesi
+                    ses.SoundLocation = "../../sesler/kazandı.wav";
+                    ses.Play();  
+                    #endregion
+
+                    DialogResult oku = MessageBox.Show("Oyunu Kazandınız Yeni Oyun için EVET, Oyunu Kapatmak için HAYIR'ı seçiniz.", "Oyun Bitti", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                    switch (oku)
+                    {
+                        case DialogResult.No: Application.Exit();
+                            break;
+                        case DialogResult.Yes: YeniOyun();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } 
+            #endregion
+            
+            #region Kaybetme Olayı
+            else
+            {
+                btnYeniOyun.BackgroundImage = Image.FromFile("../../images/üzgün.png");
+                btnYeniOyun.BackgroundImageLayout = ImageLayout.Stretch;
+                
+                        Tiklanan.BackColor = Color.Red;
+                        Tiklanan.BackgroundImage = Image.FromFile("../../images/mayin.png");
+                        Tiklanan.BackgroundImageLayout = ImageLayout.Stretch;
+                
+                #region Kaybetme sesi
+                ses.SoundLocation = "../../sesler/ağlama.wav";
+                ses.Play(); 
+                #endregion
+                
+                DialogResult oku = MessageBox.Show("Oyunu Kaybettiniz Yeni Oyun için EVET, Oyunu Kapatmak için HAYIR'ı seçiniz.", "Oyun Bitti", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                switch (oku)
+                {
+                    case DialogResult.No: Application.Exit();
+                        break;
+                    case DialogResult.Yes: YeniOyun();
+                        break;
+                    default:
+                        break;
+                }
+            } 
+            #endregion
+        } 
+
+        #endregion
+
+        #region YeniOyun Butonu
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            YeniOyun();
+        } 
+        #endregion
+
+        #region Yeni Oyun Butonu Açıklama
+        private void btnYeniOyun_MouseEnter(object sender, EventArgs e)
+        {
+            lblYeniOyun.Visible = true;
+        }
+
+        private void btnYeniOyun_MouseLeave(object sender, EventArgs e)
+        {
+            lblYeniOyun.Visible = false;
+        }
+        
+        #endregion
+
+        #region EMRE SANDIKÇI
+        
+        #endregion
+    }
+}
